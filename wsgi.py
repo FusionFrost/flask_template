c4345b0924ee550b6c8c73195b7c6cc2f4278b7a
Pavel Sh <sh@vtb.ru>
2023-08-18 21:51:44 +0300

    fix

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ from server import app
 #uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app
 #uwsgi --ini app.ini
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
